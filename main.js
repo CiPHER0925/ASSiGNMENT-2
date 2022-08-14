f6a9398b49a02da5e5cee360da62b33a267dd518
Anish Pawaskar <anishpawaskar@gmail.com>
2022-08-14 15:48:48 +0530

    Update: feat remove and complete
    Added remove and complete function so whenever user click on complete button it'll remove the delete button and when user click on delete button it will remove the li from ul.

--- main.js
+++ main.js
@@ -14,14 +14,20 @@ function addItem(e) {
     li.id = 'list-items';
     li.className = 'added-item';
 
+    //add input in li
+    let liValue = li.appendChild(document.createTextNode(input.value));
+
+    //create p
+    let p = document.createElement('p');
+    p.id = 'value';
+    p.className = 'input-value';
+    p.appendChild(liValue);
+
     //create complete button
     let completeBtn = document.createElement('button');
     completeBtn.id = 'checked';
     completeBtn.className = 'complete';
     completeBtn.appendChild(document.createTextNode('✔'));
-
-    //add input in li
-    let liValue = li.appendChild(document.createTextNode(input.value));
 
     //create delete button
     let delBtn = document.createElement('button');
@@ -29,8 +35,13 @@ function addItem(e) {
     delBtn.className = 'delete';
     delBtn.appendChild(document.createTextNode('❌'));
 
+    //create div to store button
+    let div = document.createElement('div');
+    div.className = 'buttons';
+    div.append(completeBtn, delBtn);
+
     //add complete button, input value and delete button in li
-    li.append(completeBtn, liValue, delBtn);
+    li.append(p, div);
 
     //add li into item(ul)
     item.appendChild(li);
@@ -41,5 +52,10 @@ function addItem(e) {
 
 
 function removeItem(e) {
-    
+    if(e.target.classList[0] == 'delete') {
+        e.target.parentElement.parentElement.remove();
+    }
+    if(e.target.classList[0] == 'complete') {
+        e.target.parentElement.children[1].remove();
+    }
 }
