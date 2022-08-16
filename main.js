c41a44600714fdcb1f10bda85b3086d57dd32c9d
Anish Pawaskar <anishpawaskar@gmail.com>
2022-08-17 00:07:49 +0530

    Update: input box feat and linked background image
    add feat if input box is empty then then user can't anything unless hu puts some value in input box

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let addBtn = document.getElementById('add-btn');
+let userInput = document.getElementById('user-input');
 let item = document.getElementById('items');
 
 
@@ -6,48 +7,50 @@ addBtn.addEventListener('click', addItem);
 item.addEventListener('click', removeItem);
 
 function addItem(e) {
-    //take input from user
-    let input = document.getElementById('user-input');
+    if(userInput.value != '') {
+         //take input from user
+         let input = document.getElementById('user-input');
 
-    //create li
-    let li = document.createElement('li');
-    li.id = 'list-items';
-    li.className = 'added-item';
-
-    //add input in li
-    let liValue = li.appendChild(document.createTextNode(input.value));
-
-    //create p
-    let p = document.createElement('p');
-    p.id = 'value';
-    p.className = 'input-value';
-    p.appendChild(liValue);
-
-    //create complete button
-    let completeBtn = document.createElement('button');
-    completeBtn.id = 'checked';
-    completeBtn.className = 'complete';
-    completeBtn.appendChild(document.createTextNode('✔'));
-
-    //create delete button
-    let delBtn = document.createElement('button');
-    delBtn.id = 'del-btn';
-    delBtn.className = 'delete';
-    delBtn.appendChild(document.createTextNode('❌'));
-
-    //create div to store button
-    let div = document.createElement('div');
-    div.className = 'buttons';
-    div.append(completeBtn, delBtn);
-
-    //add complete button, input value and delete button in li
-    li.append(p, div);
-
-    //add li into item(ul)
-    item.appendChild(li);
-
-    //reset value of input box
-    input.value = '';
+         //create li
+         let li = document.createElement('li');
+         li.id = 'list-items';
+         li.className = 'added-item';
+     
+         //add input in li
+         let liValue = li.appendChild(document.createTextNode(input.value));
+     
+         //create p
+         let p = document.createElement('p');
+         p.id = 'value';
+         p.className = 'input-value';
+         p.appendChild(liValue);
+     
+         //create complete button
+         let completeBtn = document.createElement('button');
+         completeBtn.id = 'checked';
+         completeBtn.className = 'complete';
+         completeBtn.appendChild(document.createTextNode('✔'));
+     
+         //create delete button
+         let delBtn = document.createElement('button');
+         delBtn.id = 'del-btn';
+         delBtn.className = 'delete';
+         delBtn.appendChild(document.createTextNode('❌'));
+     
+         //create div to store button
+         let div = document.createElement('div');
+         div.className = 'buttons';
+         div.append(completeBtn, delBtn);
+     
+         //add complete button, input value and delete button in li
+         li.append(p, div);
+     
+         //add li into item(ul)
+         item.appendChild(li);
+     
+         //reset value of input box
+         input.value = '';
+    }
 }
 
 
